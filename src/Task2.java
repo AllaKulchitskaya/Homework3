3fa5be169e2583ce1fd0854c0c23435fdf718ad3
AllaKulchitskaya <alla070@list.ru>
2022-11-01 23:37:08 +0300

    Задача 2

---
+++ src/Task2.java
@@ -0,0 +1,20 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte cat = 67;
+        System.out.println("Значение переменной cat с типом byte - " + cat);
+        short dog = -159;
+        System.out.println("Значение переменной dog с типом short - " + dog);
+        int mouse = 569;
+        System.out.println("Значение переменной mouse с типом int - " + mouse);
+        long cow = 987678965549L;
+        System.out.println("Значение переменной cow с типом long - " + cow);
+        float crocodile = 2.786f;
+        System.out.println("Значение переменной crocodile с типом float - " + crocodile);
+        double bird = 27.12;
+        System.out.println("Значение переменной bird с типом double - " + bird);
+        char bear = 27897;
+        System.out.println("Значение переменной bear с типом char - " + bear);
+        boolean fox = cat >= 80;
+        System.out.println("Значение переменной fox с типом boolean - " + fox);
+    }
+}
